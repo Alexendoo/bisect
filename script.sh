10ae0c29688b9de908e4a2644428cfd550c7a1f3
Alex Macleod <alex@macleod.io>
2021-08-22 18:20:27 +0100

    Remove more files after script.sh

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ start=${1:-$(date -I --date="2 days ago")}
 
 cargo bisect-rustc --regress non-ice --preserve --access github --start $start --script bash -- ./src/main.sh
 
-rm -f *.rlib *.pdb *.exe *.o
+rm -rf target*/ *.rlib *.rmeta *.d *.o *.pdb *.exe
