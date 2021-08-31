2440d84d2b7717a6bb2ff06c70acd0807fb135ec
Alex Macleod <alex@macleod.io>
2021-08-31 11:59:03 +0100

    Run script in temp dir

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-start=$(date -I --date="${1:-2 days ago}")
+start="$(date -I --date="${1:-2 days ago}")"
 
-cargo bisect-rustc --regress non-ice --preserve --access github --start $start --script bash -- ./src/main.sh
+cargo bisect-rustc --regress non-ice --preserve --access github --start $start --script bash -- -c 'cd $(mktemp -dp .); ../src/main.sh'
 
-rm -rf target*/ *.rlib *.rmeta *.d *.o *.pdb *.exe
+rm -rf tmp.*/
