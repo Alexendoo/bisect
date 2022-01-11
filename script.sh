b4ac5083a53de02fc9f820d21b422d75130dabb2
Alex Macleod <alex@macleod.io>
2022-01-11 17:29:35 +0000

    script.sh: don't download cargo

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@
 
 start="$(date -I --date="${1:-2 days ago}")"
 
-cargo bisect-rustc --regress non-ice --preserve --access github --start $start --script bash -- -c 'cd $(mktemp -dp .); ../src/main.sh'
+cargo bisect-rustc --without-cargo --regress non-ice --preserve --access github --start $start --script bash -- -c 'cd $(mktemp -dp .); ../src/main.sh'
 
 rm -rf tmp.*/
