0f4d641cfc71e2b3be7a7d1ad04f72836cb84fd9
Alex Macleod <alex@macleod.io>
2022-09-01 12:24:01 +0000

    workaround file not found

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cargo bisect-rustc --without-cargo --regress non-ice --preserve --access github --script bash "$@" -- -c 'cd $(mktemp -dp .); ../src/main.sh'
+cargo-bisect-rustc --without-cargo --regress non-ice --preserve --access github --script /bin/bash "$@" -- -c 'cd $(mktemp -dp .); ../src/main.sh'
 
 rm -rf tmp.*/
